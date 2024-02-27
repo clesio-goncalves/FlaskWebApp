cfa68023c0f68ad691ef4cc1fc7659b9392d9270
Lucas Bezerra <lbezerradev@gmail.com>
2024-02-27 12:38:32 -0300

    feat: formart with black

--- app.py
+++ app.py
@@ -1,25 +1,27 @@
 #################################################################
-from decouple import config
-from flask import Flask, render_template, redirect, request
-from datetime import datetime
 import os
-import joblib
+from datetime import datetime
+
 import cv2
+import joblib
 import numpy as np
 import pandas as pd
 import skimage
-from tensorflow.keras.preprocessing.image import img_to_array, load_img
+from decouple import config
+from flask import Flask, render_template, redirect, request
 from tensorflow.keras.applications.inception_v3 import preprocess_input
 from tensorflow.keras.models import model_from_json
+from tensorflow.keras.preprocessing.image import img_to_array, load_img
+
 #################################################################
 
 app = Flask(__name__)
 
-RESET_VARIABLES = config('RESET_VARIABLES', False, cast=bool)
+RESET_VARIABLES = config("RESET_VARIABLES", False, cast=bool)
 
 #################################################################
-json_path = 'models/inception_v3.json'
-model_path = 'models/inception_v3.h5'
+json_path = "models/inception_v3.json"
+model_path = "models/inception_v3.h5"
 
 classes = []
 diretorio_base = "static/imagens/"
@@ -61,6 +63,7 @@ model.load_weights(model_path)
 loaded_model = joblib.load("models/random_forest_model.joblib")
 #################################################################
 
+
 def resetar_variaveis():
     if not RESET_VARIABLES:
         return
@@ -73,7 +76,9 @@ def resetar_variaveis():
     houve_classificacao = False
     img_invalidas = 0
     img_removidas = 0
-    df = pd.DataFrame(columns=['nome_imagem', 'classe_predicao', 'porcentagem_predicao'])
+    df = pd.DataFrame(
+        columns=["nome_imagem", "classe_predicao", "porcentagem_predicao"]
+    )
     filenames = []
 
 
@@ -118,6 +123,7 @@ def inicia_lamina():
     resetar_variaveis()
     return redirect("/adiciona_lamina.html")
 
+
 def consulta_predicao_dataset(lamina):
     num_linhas = df_laminas.shape[0]
     predicao_existente = "Indefinida"
@@ -130,6 +136,7 @@ def consulta_predicao_dataset(lamina):
 
     return predicao_existente
 
+
 def atualiza_predicao_dataset(lamina):
 
     predicao_existente = consulta_predicao_dataset(lamina)
@@ -137,27 +144,34 @@ def atualiza_predicao_dataset(lamina):
     # Só atualiza se não for positiva
     if not predicao_existente == "Positiva":
         # Verificando se existe classe positiva no DataFrame
-        existe_classe_positiva = (df['classe_predicao'] == 'Positiva').any()
+        existe_classe_positiva = (df["classe_predicao"] == "Positiva").any()
 
         if existe_classe_positiva:
             # Alterando a predição da lâmina para positiva
-            df_laminas.loc[df_laminas['nome_lamina'] == lamina, 'classe_predicao'] = "Positiva"
+            df_laminas.loc[df_laminas["nome_lamina"] == lamina, "classe_predicao"] = (
+                "Positiva"
+            )
         else:
             # Alterando a predição da lâmina para negativo
-            df_laminas.loc[df_laminas['nome_lamina'] == lamina, 'classe_predicao'] = "Negativa"
+            df_laminas.loc[df_laminas["nome_lamina"] == lamina, "classe_predicao"] = (
+                "Negativa"
+            )
 
         # alterando a data
-        df_laminas.loc[df_laminas['nome_lamina'] == lamina, 'data_atualizacao'] = datetime.now()
+        df_laminas.loc[df_laminas["nome_lamina"] == lamina, "data_atualizacao"] = (
+            datetime.now()
+        )
 
         df_laminas.to_csv(diretorio_base + "dataset.csv", index=False)
+
 
 def filtrar_dataframe():
     global df
 
     # Filtrando apenas as predições positivas
-    df_positivas = df[df['classe_predicao'] == 'Positiva']
+    df_positivas = df[df["classe_predicao"] == "Positiva"]
 
-    dez_maiores = None #inicializa variável
+    dez_maiores = None  # inicializa variável
 
     # Selecionando as dez maiores porcentagens de predições
     if df_positivas.shape[0] > 0:
@@ -182,7 +196,8 @@ def filtrar_dataframe():
 
     return dez_maiores
 
-@app.route('/classificacao.html')
+
+@app.route("/classificacao.html")
 def classificacao():
 
     global classes, img_removidas, img_invalidas, pasta_lamina, houve_classificacao
@@ -190,11 +205,11 @@ def classificacao():
     if pasta_lamina == "":
         return inicia_lamina()
 
-    dez_maiores = filtrar_dataframe() # seleciona as 10 maiores predições positivas
+    dez_maiores = filtrar_dataframe()  # seleciona as 10 maiores predições positivas
     num_linhas = dez_maiores.shape[0]
 
     # Verificando a predição com base no nome da lâmina
-    lamina=pasta_lamina.split("/")[-1]
+    lamina = pasta_lamina.split("/")[-1]
     predicao_existente = consulta_predicao_dataset(lamina)
 
     return render_template(
@@ -215,7 +230,8 @@ def inicia_classificacao():
     resetar_variaveis()
     return inicia_lamina()
 
-@app.route('/classificacao/<lamina>')
+
+@app.route("/classificacao/<lamina>")
 def classificacao_lamina(lamina):
     global pasta_lamina
 
@@ -223,6 +239,7 @@ def classificacao_lamina(lamina):
     pasta_lamina = diretorio_base + lamina
 
     return redirect("/classificacao.html")
+
 
 def ler_imagem():
     global filenames
@@ -237,10 +254,14 @@ def ler_imagem():
 
     return imagens
 
-#Entensões permitidas png, jpg e jpeg
-EXTENSOES = set(['jpg', 'jpeg', 'png', 'tif'])
+
+# Entensões permitidas png, jpg e jpeg
+EXTENSOES = set(["jpg", "jpeg", "png", "tif"])
+
+
 def formatos_permitidos(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in EXTENSOES
+    return "." in filename and filename.rsplit(".", 1)[1].lower() in EXTENSOES
+
 
 # Extrai características com o GLCM
 def calculate_glcm_features(image):
@@ -248,16 +269,19 @@ def calculate_glcm_features(image):
     gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
     # Calcule a matriz GLCM
-    glcm = skimage.feature.graycomatrix(gray_image, distances=[1], angles=[0], levels=256, symmetric=True, normed=True)
+    glcm = skimage.feature.graycomatrix(
+        gray_image, distances=[1], angles=[0], levels=256, symmetric=True, normed=True
+    )
 
     # Calcule as propriedades da matriz GLCM
-    contrast = skimage.feature.graycoprops(glcm, 'contrast')
-    dissimilarity = skimage.feature.graycoprops(glcm, 'dissimilarity')
-    homogeneity = skimage.feature.graycoprops(glcm, 'homogeneity')
-    energy = skimage.feature.graycoprops(glcm, 'energy')
-    correlation = skimage.feature.graycoprops(glcm, 'correlation')
+    contrast = skimage.feature.graycoprops(glcm, "contrast")
+    dissimilarity = skimage.feature.graycoprops(glcm, "dissimilarity")
+    homogeneity = skimage.feature.graycoprops(glcm, "homogeneity")
+    energy = skimage.feature.graycoprops(glcm, "energy")
+    correlation = skimage.feature.graycoprops(glcm, "correlation")
 
     return contrast, dissimilarity, homogeneity, energy, correlation
+
 
 # Verifica se a imagem é de microscopia, com base no calculo GLCM. Remove a imagem caso não seja de microscopia
 def verifica_imagem_upload():
@@ -267,36 +291,43 @@ def verifica_imagem_upload():
 
     for filename in imagens:
         image = cv2.imread(filename)
-        features = calculate_glcm_features(image) # Extrai as características GLCM da imagem
-        features = np.reshape(features, (1, -1)) # Redimensiona as características para que tenham a mesma forma usada durante o treinamento
-        prediction = loaded_model.predict(features) # Faz a predição usando o modelo treinado
+        features = calculate_glcm_features(
+            image
+        )  # Extrai as características GLCM da imagem
+        features = np.reshape(
+            features, (1, -1)
+        )  # Redimensiona as características para que tenham a mesma forma usada durante o treinamento
+        prediction = loaded_model.predict(
+            features
+        )  # Faz a predição usando o modelo treinado
         print("Predição Ramdom Forest: ", prediction)
 
         # Classificar com base no limiar
-        if prediction >= 0.5: # limiar
+        if prediction >= 0.5:  # limiar
             continue
         else:
             print("A imagem [{}] não é uma imagem de microscopia.".format(filename))
-            filenames.remove(filename) # remove a imagem da lista
-            os.remove(filename) # remove a imagem da pasta
-            img_removidas+=1
+            filenames.remove(filename)  # remove a imagem da lista
+            os.remove(filename)  # remove a imagem da pasta
+            img_removidas += 1
 
-@app.route('/add_lamina', methods=['POST'])
+
+@app.route("/add_lamina", methods=["POST"])
 def add_lamina():
 
     global pasta_lamina, pasta_existente, df, df_laminas
 
-    nome_lamina = request.form['nome_lamina']
+    nome_lamina = request.form["nome_lamina"]
     pasta_lamina = diretorio_base + nome_lamina.upper()
     caminho_csv = f'{pasta_lamina + "/" + pasta_lamina.split("/")[-1]}.csv'
 
     if os.path.isdir(pasta_lamina):  # pasta ja existe
         pasta_existente = True
-        df = pd.read_csv(caminho_csv) # ler o CSV
+        df = pd.read_csv(caminho_csv)  # ler o CSV
         return redirect("/adiciona_lamina.html")
     else:
         os.mkdir(pasta_lamina)  # cria a pasta
-        df.to_csv(caminho_csv, index=False) # cria o CSV
+        df.to_csv(caminho_csv, index=False)  # cria o CSV
 
         df_laminas = pd.concat(
             [
@@ -321,22 +352,25 @@ def add_lamina():
         pasta_existente = False
         return redirect("/classificacao.html")
 
-@app.route('/predict', methods=['POST'])
+
+@app.route("/predict", methods=["POST"])
 def predict():
     global classes, filenames, model, img_invalidas, pasta_lamina, df, houve_classificacao
 
-    arquivos = request.files.getlist('arquivo')  # Obtem uma lista de arquivos enviados
+    arquivos = request.files.getlist("arquivo")  # Obtem uma lista de arquivos enviados
 
     filenames = []
     img_invalidas = 0
     for arquivo in arquivos:
-        if arquivo and formatos_permitidos(arquivo.filename):  # Verifica o formato da imagem
+        if arquivo and formatos_permitidos(
+            arquivo.filename
+        ):  # Verifica o formato da imagem
             nome_arquivo = arquivo.filename
             arquivo_path = os.path.join(pasta_lamina, nome_arquivo)
             arquivo.save(arquivo_path)
             filenames.append(arquivo_path)
         else:
-            img_invalidas += 1 # formato inválido
+            img_invalidas += 1  # formato inválido
 
     print(filenames)
 
@@ -345,9 +379,9 @@ def predict():
 
     imagens = ler_imagem()  # Pré-processamento das imagens
 
-    classe=""
-    porcentagem=""
-    nova_linha={}
+    classe = ""
+    porcentagem = ""
+    nova_linha = {}
     for i, img in enumerate(imagens):
         prediction = model.predict(img)  # Predição
         temp = prediction
@@ -355,16 +389,16 @@ def predict():
         if prediction[[0]] == 1:
             classe = "Positiva"
             porcentagem = round(temp[0][0] * 100, 2)
-            #classe = f"Classe: Positiva ({porcentagem}%)"
+            # classe = f"Classe: Positiva ({porcentagem}%)"
         else:
             classe = "Negativa"
             porcentagem = round((1 - temp[0][0]) * 100, 2)
 
         # Dicionário com os dados da nova linha a ser adicionada
         nova_linha = {
-            'nome_imagem': filenames[i],
-            'classe_predicao': classe,
-            'porcentagem_predicao': porcentagem
+            "nome_imagem": filenames[i],
+            "classe_predicao": classe,
+            "porcentagem_predicao": porcentagem,
         }
 
         # Adicionando a nova linha ao DataFrame
@@ -382,8 +416,7 @@ def predict():
 
     houve_classificacao = True
 
-    return redirect('/classificacao.html')
-
+    return redirect("/classificacao.html")
 
 
 @app.route("/send_image")
@@ -434,9 +467,7 @@ def predict_send_image():
         else:
             img_invalidas += 1  # formato inválido
 
-
     imagens = ler_imagem()  # Pré-processamento das imagens
-
 
     for i, img in enumerate(imagens):
         prediction = model.predict(img)  # Predição
@@ -469,4 +500,5 @@ def predict_send_image():
 
 if __name__ == "__main__":
     from waitress import serve
+
     serve(app, host="0.0.0.0", port=5000)
